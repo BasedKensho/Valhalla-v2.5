21a441085745a1e2b857ab713fb5c60327420f99
BasedKensho <lukenymedia@gmail.com>
2025-06-01 17:24:02 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { DemoHeroGeometric } from '@/components/Hero';
+import { TimelineDemo } from '@/components/TimelineDemo';
+import { BorderBeamDemo } from '@/components/ui/border-beam-demo';
+import { BackgroundPaths } from '@/components/ui/background-paths';
+
+function App() {
+  return (
+    <div className="w-full h-full">
+      <DemoHeroGeometric />
+      <BorderBeamDemo />
+      <TimelineDemo />
+      <BackgroundPaths />
+    </div>
+  );
+}
+
+export default App;
